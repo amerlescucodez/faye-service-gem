7ab92abcafda756320773dfe53328c9efd12f1c6
Andrei Merlescu <andrei+github@merlescu.net>
2019-08-24 10:42:02 -0400

    Added more useful comments to the faye_service.rb file

--- lib/faye_service.rb
+++ lib/faye_service.rb
@@ -9,7 +9,15 @@ module FayeService
     Dir.pwd
   end #/def
 
-  # Shortcut to loading the configs
+  # Loads the config/faye_service.yml file and returns its YAML instance
+  #
+  # config/faye_service.yml
+  # =======================
+  # faye_service: 
+  #   url: http://0.0.0.0:8080/faye
+  #   auth_token: APPLICATION_TOKEN
+  #   auth_service: SERVICE_NAME
+  #   return_response: no
   def FayeService.config
     # Requires inside the rails/etc application a file called config/faye_service.yml
     if File.exists?(File.join(path, "config", "faye_service.yml"))
@@ -20,27 +28,32 @@ module FayeService
     end #/if-else
   end #/def
 
-  # Shortcut for accessing URL
+  # Reads the YAML config file and extracts the faye_service->url definition
+  # => If undefined, raise a Ruby standard error since this gem depends on its definition
   def FayeService.url
     FayeService.config["faye_service"]["url"]
   end #/def
 
-  # Shortcut for accessing auth token
+  # Reads the YAML config file and extracts the faye_service->auth_token definition
+  # => If undefined, return empty string
   def FayeService.auth_token
     FayeService.config["faye_service"]["auth_token"]
   end #/def
 
-  # Shortcut for access auth service
+  # Reads the YAML config file and extracts the faye_service->auth_service definition
+  # => If undefined, return empty string
   def FayeService.auth_service
     FayeService.config["faye_service"]["auth_service"]
   end #/def
 
-  # Shortcut for accessing http response
+  # Reads the YAML config file and extracts the faye_service->return response definition
+  # => If "yes" then return true
+  # => If "no" or nil then return fals
   def FayeService.return_http_response?
     (FayeService.config["faye_service"]["return_response"] == "yes") ? true : false
   end #/def
 
-  # Shortcut for publishing a new message
+  # alias of FayeService::Publisher.publish inside lib/faye_service/publisher.rb
   def publish(channel, message)
     FayeService::Publisher.publish(channel, message)
   end #/def
