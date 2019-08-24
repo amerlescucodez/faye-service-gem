e269263e955f37f780350a6347685f77c825c959
Andrei Merlescu <andrei+github@merlescu.net>
2019-08-24 10:43:08 -0400

    Added documentation for channel publishing

--- lib/faye_service/publisher.rb
+++ lib/faye_service/publisher.rb
@@ -1,6 +1,24 @@
 module FayeService
 	module Publisher
 		class << self
+			# Creates a new HTTP Request with the channel and message to broadcast to any potential channel subscribers
+      #
+      # channel => String
+      # => Channel must be structured like: 
+      #    /channel/name
+      #    /parent/child
+      #    /parent/child1/child2
+      #    /parent/child1/child2/child3
+      #    /parent/child1/child2/child3/child4
+      #    /parent/child1/child2/child3/child4/child5
+      #    /parent/child1/child2/child3/child4/child5/child6
+      # => Channels can only go 6 children deep
+      # => Channels must be between 3 and 32 characters in length
+      # => Channels can only contain abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUBWXYV0123456789-
+      #
+      # message => String, Array, or Hash
+      # => Will be parsed into JSON
+      # => Should be less than 10KB in size (no programmed limit)
 			def publish(channel,message)
 				if channel =~ %r{^(/[\w-]{3,32}\/[\w-]{3,32}?(\/[\w-]{3,32})?(\/[\w-]{3,32})?(\/[\w-]{3,32})?(\/[\w-]{3,32})?(\/[\w-]{3,32})?)$}
 					message = {
