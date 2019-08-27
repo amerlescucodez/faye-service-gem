60f5c2d9bf6c82e6840a2a31d7c9954cd060baee
Andrei Merlescu <andrei+github@merlescu.net>
2019-08-26 22:48:09 -0400

    Switched preference setting from yaml file which can be stored insecurely in git to initalizer style that will allow environment variables to be used instead

--- lib/faye_service.rb
+++ lib/faye_service.rb
@@ -5,16 +5,8 @@ require 'logging'
 
 module FayeService
 
-  @logger = nil
+  @config = {url: nil, auth_token: nil, auth_service: nil}
 
-  # Shortcut to the logger variable
-  def FayeService.logger
-    if @logger.nil?
-      @logger = Logging.logger['FayeService']
-      @logger.level = :"#{config["faye_service"]["log_level"]||"warn"}"
-    end
-    @logger
-  end #/def
 
   # Shortuct for current working directory
   def FayeService.path
@@ -29,37 +21,43 @@ module FayeService
   #   url: http://0.0.0.0:8080/faye
   #   auth_token: APPLICATION_TOKEN
   #   auth_service: SERVICE_NAME
-  #   return_response: no
   def FayeService.config
-    # Requires inside the rails/etc application a file called config/faye_service.yml
-    if File.exists?(File.join(path, "config", "faye_service.yml"))
-      config = YAML.load_file(File.join(path, "config","faye_service.yml"))
-      return config
-    else
-      raise(StandardError,"No faye_service.yml file inside the config directory.")
-    end #/if-else
+    return @config
   end #/def
 
-  # Reads the YAML config file and extracts the faye_service->url definition
-  # => If undefined, raise a Ruby standard error since this gem depends on its definition
+  def FayeService.set_config(new_config)
+    @config = new_config
+  end #/def
+
+  def FayeService.set_url(url)
+    @config[:url] = url
+  end #/def
+
+  def FayeService.set_auth_token(auth_token)
+    @config[:auth_token] = auth_token
+  end #/def
+
+  def FayeService.set_auth_service(auth_service)
+    @config[:auth_service] = auth_service
+  end #/def
+
   def FayeService.url
-    FayeService.config["faye_service"]["url"]
+    FayeService.config[:url]
   end #/def
 
-  # Reads the YAML config file and extracts the faye_service->auth_token definition
-  # => If undefined, return empty string
   def FayeService.auth_token
-    FayeService.config["faye_service"]["auth_token"]
+    FayeService.config[:auth_token]
   end #/def
 
-  # Reads the YAML config file and extracts the faye_service->auth_service definition
-  # => If undefined, return empty string
   def FayeService.auth_service
-    FayeService.config["faye_service"]["auth_service"]
+    FayeService.config[:auth_service]
   end #/def
 
   # alias of FayeService::Publisher.publish inside lib/faye_service/publisher.rb
   def FayeService.publish(channel, message)
+    raise(StandardError,"Undefined config.url") if FayeService.url.nil?
+    raise(StandardError,"Undefined config.auth_token") if FayeService.auth_token.nil?
+    raise(StandardError,"Undefined config.auth_service") if FayeService.auth_service.nil?
     FayeService::Publisher.publish(channel, message)
   end #/def
 
